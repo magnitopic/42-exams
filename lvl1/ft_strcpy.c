8aab2a76a566f9a36b812fdb8153b899d20f10e5
magnitopic <magnitopic@gmail.com>
2023-05-05 16:36:22 +0200

    strcpy

---
+++ lvl1/ft_strcpy.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+char	*ft_strcpy(char *s1, char *s2)
+{
+	int i = 0;
+	while (s2[i])
+	{
+		s1[i] = s2[i];
+		i++;
+	}
+	s1[i] = '\0';
+	return s1;
+}
+
+/* int main(void)
+{
+	char *s2= "hi";
+	char s1[3];
+	write(1, ft_strcpy(s1, s2), 2);
+	return 0;
+} */
