a599f8a0ac7429c4734872a7d31233700f204cc8
magnitopic <magnitopic@gmail.com>
2023-06-27 16:53:41 +0200

    simpler gnl

---
+++ exam-03/get_next_line/README.md
@@ -0,0 +1,5 @@
+# GNL
+
+```bash
+gcc get_next_line.c -D BUFFER_SIZE=42
+```
--- exam-03/get_next_line/get_next_line.c
+++ exam-03/get_next_line/get_next_line.c
@@ -1,23 +1,20 @@
 #include "get_next_line.h"
+#include <stdio.h>
 #include <fcntl.h>
 
 char *get_next_line(int fd)
 {
 	int i = 0;
-	int rd;
 	char character;
-	char *buffer = malloc(1000000);
+	char *buffer = malloc(100000000);
 
 	if (BUFFER_SIZE < 1)
-	{
-		free(buffer);
-		return (NULL);
-	}
-	while ((rd =read(fd, &character, BUFFER_SIZE - BUFFER_SIZE + 1))  > 0)
+		return (free(buffer), NULL);
+	while (read(fd, &character, BUFFER_SIZE / BUFFER_SIZE) > 0)
 	{
 		buffer[i++] = character;
 		if (character == '\n')
-			break ;
+			break;
 	}
 	buffer[i] = '\0';
 	if (i == 0)
@@ -25,23 +22,17 @@ char *get_next_line(int fd)
 	return buffer;
 }
 
-int main (void)
+
+int main(void)
 {
-	int fd =open ("get_next_line.h", O_RDONLY);
+	int fd = open("get_next_line.h", O_RDONLY);
 	char *test = get_next_line(fd);
 	while (test)
 	{
 		printf("%s", test);
 		test = get_next_line(fd);
 	}
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
-	printf("%s",get_next_line(fd));
+	printf("%s", get_next_line(fd));
+
 	return (0);
 }
