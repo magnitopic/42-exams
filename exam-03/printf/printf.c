a3cd174a88c1cc7bdd0c4b063aef11561ca52131
magnitopic <magnitopic@gmail.com>
2023-06-28 17:02:24 +0200

    fixed printf type bug

--- exam-03/printf/printf.c
+++ exam-03/printf/printf.c
@@ -1,55 +1,57 @@
-#include <unistd.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
 
-void	put_str(char *str, int *len)
+void put_str(char *str, int *len)
 {
 	if (!str)
-		str = "(null)";
+		str = "(NULL)";
 	while (*str)
 		*len += write(1, str++, 1);
 }
 
-void put_digit(long long int number, int base, int *len)
+void put_num(long long int num, int base, int *len)
 {
-	char *hex = "0123456789abcdef";
-
-	if (number < 0)
+	char *hex= "0123456789abcdef";
+	
+	if (num < 0)
 	{
-		number *= -1;
-		*len  += write(1, "-", 1);
+		num *= -1;
+		*len += write(1, "-", 1);
 	}
-	if (number >= base)
-		put_digit((number / base), base, len);
-	*len += write(1, &hex[number % base], 1);
+	if (num >= base)
+		put_num((num / base), base, len);
+	*len += write(1, &hex[num % base], 1);
 }
 
-int ft_printf(const char *format, ... )
+int ft_printf(const char *str, ...)
 {
 	va_list args;
 	int len = 0;
-	
-	va_start(args, format);
-	while (*format)
+
+	va_start(args, str);
+	while (*str)
 	{
-		if ((*format == '%' && (*(format + 1) == 's' || *(format + 1) == 'd' || *(format + 1) == 'x')))
+		if (*str == '%' && (*(str + 1) == 's' || *(str + 1) == 'd' || *(str +1) == 'x'))
 		{
-			format++;
-			if (*format == 's')
+			str++;
+			if (*str == 's')
 				put_str(va_arg(args, char *), &len);
-			else if (*format == 'd')
-				put_digit(va_arg(args, long long int), 10, &len);
+			else if (*str == 'd')
+				put_num((long long int)va_arg(args, int), 10, &len);
 			else
-				put_digit(va_arg(args, long long int), 16, &len);
+				put_num((long long int)va_arg(args, unsigned int), 16, &len);
 		}
 		else
-			len += write(1, format, 1);
-		format++;
+			len += write(1, str, 1);
+		str++;
 	}
 	return (va_end(args), len);
 }
 
+
 int main(void)
 {
-	ft_printf("Hello%s", " There\n");
-	return 0;
+	printf(" - %d\n", printf("Hello%s %d %d %x", " There", -42, -89, 6463));
+	printf(" - %d\n", ft_printf("Hello%s %d %d %x", " There", -42, -89, 6463));
 }
