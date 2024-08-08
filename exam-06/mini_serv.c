6bb11d05e3b0bf98563fd15f2a9a26c71f832c70
Alejandro Magnitopic <magnitopic@gmail.com>
2024-08-08 09:54:52 +0200

    fixed minor errors miniserv

--- exam-06/mini_serv.c
+++ exam-06/mini_serv.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <netinet/in.h>
 
 typedef struct s_client
 {
@@ -12,9 +12,9 @@ typedef struct s_client
 } t_client;
 
 t_client clients[1024];
-fd_set read_set, write_set, current;
+fd_set write_set, read_set, current;
 int gid = 0, maxfd = 0;
-char recv_buff[300000], send_buff[30000];
+char recv_buff[300000], send_buff[300000];
 
 void err(char *msg)
 {
@@ -26,22 +26,19 @@ void err(char *msg)
 	exit(1);
 }
 
-
 void msg_all(int except)
 {
 	for (int fd = 0; fd <= maxfd; fd++)
-	{
 		if (FD_ISSET(fd, &write_set) && fd != except)
-			if (send(fd, &send_buff, strlen(send_buff), 0) == -1)
+			if (send(fd, send_buff, strlen(send_buff), 0) == -1)
 				err(NULL);
-	}
 }
 
 int main(int argc, char **argv)
 {
 	if (argc != 2)
 		err("Wrong number of arguments");
-	
+
 	struct sockaddr_in serveraddr;
 	socklen_t len;
 	int serverfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -59,19 +56,18 @@ int main(int argc, char **argv)
 
 	if (bind(serverfd, (const struct sockaddr *)&serveraddr, sizeof(serveraddr)) == -1 || listen(serverfd, 100) == -1)
 		err(NULL);
-
 	while (1)
 	{
-		read_set = write_set = current;
+		write_set = read_set = current;
 		if (select(maxfd + 1, &read_set, &write_set, 0, 0) == -1) continue;
-
+		
 		for (int fd = 0; fd <= maxfd; fd++)
 		{
 			if (FD_ISSET(fd, &read_set))
 			{
 				if (fd == serverfd)
 				{
-					int clientfd = accept(serverfd, (struct sockaddr *)&serveraddr, &len);
+					int clientfd = accept(serverfd, (struct sockaddr*)&serveraddr, &len);
 					if (clientfd == -1) continue;
 					if (clientfd > maxfd) maxfd = clientfd;
 					clients[clientfd].id = gid++;
@@ -92,24 +88,23 @@ int main(int argc, char **argv)
 					}
 					else
 					{
-						for (int i = 0, j = strlen(clients[fd].msg); i < rec; j++)
+						for (int i = 0, j = strlen(clients[fd].msg); i < rec; i++, j++)
 						{
-							clients[fd].msg[j] = recv_buff[j];
+							clients[fd].msg[j] = recv_buff[i];
 							if (clients[fd].msg[j] == '\n')
 							{
 								clients[fd].msg[j] = '\0';
-								snprintf(send_buff, strlen(send_buff), "client %d: %s\n", clients[fd].id, clients[fd].msg);
+								sprintf(send_buff, "client %d: %s\n", clients[fd].id, clients[fd].msg);
 								msg_all(fd);
-								bzero(clients[fd].msg, strlen(clients[fd].msg));
+								bzero(clients[fd].msg, sizeof(clients[fd].msg));
 								j = -1;
 							}
 						}
 					}
-					
 				}
 				break;
 			}
 		}
-	}
+	}	
+	return (0);
 }
-
